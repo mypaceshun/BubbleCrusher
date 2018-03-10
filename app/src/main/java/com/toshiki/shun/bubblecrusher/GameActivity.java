5fd270ac67238020e59dd07838e5f587592177b5
Ochiai5042 <ochat3is@engs.tamagawa.ac.jp>
2018-03-10 19:42:37 +0900

    画面遷移できたふぅ～～～～～！！！！

---
+++ app/src/main/java/com/toshiki/shun/bubblecrusher/GameActivity.java
@@ -0,0 +1,17 @@
+package com.toshiki.shun.bubblecrusher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by toshiki on 2018/03/10.
+ */
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
