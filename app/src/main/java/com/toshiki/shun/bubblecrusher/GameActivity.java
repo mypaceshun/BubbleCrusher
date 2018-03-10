80a2949e779207c1d06ffac442d8dd2560592bbf
shun <shun@isasaka.jp>
2018-03-10 20:49:00 +0900

    指定秒後にレイアウトが変わるようになった
    activity_start から10秒後にactivity_resultに変わるようになった

--- app/src/main/java/com/toshiki/shun/bubblecrusher/GameActivity.java
+++ app/src/main/java/com/toshiki/shun/bubblecrusher/GameActivity.java
@@ -1,17 +1,48 @@
 package com.toshiki.shun.bubblecrusher;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by toshiki on 2018/03/10.
  */
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+    private Handler handler = new Handler();
+    private long count = 0;
+    private int period = 1000;
+    private int limit = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        this.init();
     }
+    public void init(){
+        count = 0;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                count++;
+                Log.d("count", "count :" + count);
+                if (count > limit) {
+                    changeResultLayout();
+                }
+                handler.postDelayed(this, period);
+            }
+        }, period);
 
+    }
+    public void changeResultLayout(){
+        handler.removeCallbacksAndMessages(null);
+        setContentView(R.layout.activity_result);
+
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
 }
