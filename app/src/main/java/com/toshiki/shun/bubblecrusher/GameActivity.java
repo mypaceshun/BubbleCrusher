50ddc26a411a5180d14ad850ecf531c7fbe530d8
shun <shun@isasaka.jp>
2018-03-10 21:17:11 +0900

    画面遷移ほぼ完璧

--- app/src/main/java/com/toshiki/shun/bubblecrusher/GameActivity.java
+++ app/src/main/java/com/toshiki/shun/bubblecrusher/GameActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by toshiki on 2018/03/10.
@@ -15,30 +16,41 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private long count = 0;
     private int period = 1000;
     private int limit = 10;
+    private Runnable run;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
         this.init();
     }
     public void init(){
+        setContentView(R.layout.activity_game);
         count = 0;
-        handler.postDelayed(new Runnable() {
+        run = new Runnable() {
             @Override
             public void run() {
                 count++;
                 Log.d("count", "count :" + count);
-                if (count > limit) {
+                if (count >= limit) {
                     changeResultLayout();
+                }else {
+                    handler.postDelayed(this, period);
                 }
-                handler.postDelayed(this, period);
             }
-        }, period);
+        };
+        handler.postDelayed(run, period);
 
     }
     public void changeResultLayout(){
-        handler.removeCallbacksAndMessages(null);
+        handler.removeCallbacks(run);
+        run = null;
         setContentView(R.layout.activity_result);
+        Button retry_buttom = (Button)findViewById(R.id.retry_botton);
+        retry_buttom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
 
     }
     @Override
