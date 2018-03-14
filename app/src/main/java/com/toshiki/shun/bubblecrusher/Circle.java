e78daf54e48a3582c86a6954fa69d2c54f6b43b9
Shun Kawai <shun@osstech.co.jp>
2018-03-14 17:54:09 +0900

    [Circle.java]速度加速度に対応

--- app/src/main/java/com/toshiki/shun/bubblecrusher/Circle.java
+++ app/src/main/java/com/toshiki/shun/bubblecrusher/Circle.java
@@ -5,13 +5,24 @@ package com.toshiki.shun.bubblecrusher;
  */
 
 public class Circle {
-    // TODO privateにする
     public int x;
     public int y;
+    public int vx;
+    public int vy;
+    public int ax;
+    public int ay;
     public int radius;
+    public int color;
+
     public Circle(int x, int y, int radius) {
         this.x = x;
         this.y= y;
         this.radius = radius;
+    }
+    public void step() {
+        this.x += this.vx;
+        this.y += this.vy;
+        this.vx += this.ax;
+        this.vy += this.ay;
     }
 }
